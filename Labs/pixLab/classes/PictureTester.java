e356b114639f039e9fa22204325d3b37bb0027b2
Mathew Varughese <mat@imginconline.com>
2016-02-04 18:53:37 -0500

    Not sure what I just did

--- Labs/pixLab/classes/PictureTester.java
+++ Labs/pixLab/classes/PictureTester.java
@@ -112,6 +112,27 @@ public class PictureTester
     // uncomment a call here to run a test
     // and comment out the ones you don't want
     // to run
+    //testZeroBlue();
+    //repairTemple();
+    //testKeepOnlyBlue();
+    //testKeepOnlyRed();
+    //testKeepOnlyGreen();
+    //testNegate();
+    //testGrayscale();
+    //testFixUnderwater();
+    //testMirrorVertical();
+    //testMirrorTemple();
+    //testMirrorArms();
+    //testMirrorGull();
+    //testMirrorDiagonal();
+    //testCollage();
+    //testCopy();
+    //testEdgeDetection();
+   // testEdgeDetection2();
+    //testChromakey();
+    //testEncodeAndDecode();
+    //testGetCountRedOverValue(250);
+    //testSetRedToHalfValueInTopHalf();
     //testClearBlueOverValue(200);
     //testGetAverageForColumn(0);
     
