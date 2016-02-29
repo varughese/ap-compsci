9db0573cfc9cde57dd3ab65bbe42bfddf5fb89e9
Mathew Varughese <mat@imginconline.com>
2016-02-29 16:53:54 -0500

    insert method finish

--- Chap 14/Test/InsertionSort.java
+++ Chap 14/Test/InsertionSort.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class InsertionSort {
    public static void insert(int[] a, int n, int x) {
-      int i = 1;
-      while(i < n && a[i] > a[i-1]) {
-         a[i+1] = a[i+1];
-         i++;
+      int i = n;
+      while(i > 0 && x < a[i-1]) {
+         a[i] = a[i-1];
+         i--;
       }
       
       a[i] = x;
@@ -22,10 +22,10 @@ public class InsertionSort {
       sort(a, n+1);
    }  
    
-   public static int[] x = {2,4,5,1,3,5,7,2,9,8};
+   public static int[] x = {2,4,5,1,3,1,9,2,9,8};
    
    public static void main(String[] args) {
-      insert(x, 3, 1);
+      sort(x, 1);
       System.out.println(Arrays.toString(x));
    }
 }
