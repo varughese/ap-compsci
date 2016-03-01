0df33afc81f771f8ae3cd55a026073da25237d4a
Mathew Varughese <mat@imginconline.com>
2016-02-29 22:06:57 -0500

    jawn

--- Chap 14/Test/Prob5.java
+++ Chap 14/Test/Prob5.java
@@ -32,13 +32,50 @@ public class Prob5 {
       return x;
    }
    
+   public static ArrayList<String> mergeSort(ArrayList<String> list) {
+      int n = list.size();
+      ArrayList<String> temp = new ArrayList<String>();
+      recursiveSort(list, 0, n-1, temp);
+      return temp;
+   }
+   
+   private static void recursiveSort(ArrayList<String> a, int from, int to, ArrayList<String> temp) {
+      if(to - from < 2) {
+         if(to > from && a.get(to).compareToIgnoreCase(a.get(from)) > 0) {
+            String t = a.get(to); a.set(to, a.get(from)); a.set(from, t);
+         }
+      } else {
+         int middle = (from + to) / 2;
+         recursiveSort(a, from, middle, temp);
+         recursiveSort(a, middle + 1, to, temp);
+         ArrayList<String> a1 = new ArrayList<String>();
+         ArrayList<String> a2 = new ArrayList<String>();
+         for(int i = from; i <= middle; i++) {
+            a1.add(a.get(i));
+         }
+         for(int j = middle + 1; j <= to; j++) {
+            a2.add(a.get(j));
+         }
+         ArrayList<String> res = merge(a1, a2);
+          for(int k = from; k <= to; k++) {
+             temp.add(res.get(k));
+          }
+      }
+   }
+   
    public static void main(String[] args) {
       ArrayList<String> list1 = new ArrayList<String>();
       list1.add("Adam"); list1.add("Bob"); list1.add("Dylan"); list1.add("Frog");
       ArrayList<String> list2 = new ArrayList<String>();
       list2.add("Charles"); list2.add("Dan"); list2.add("Eggs"); list2.add("Ryan"); list2.add("Jason"); list2.add("ZA"); list2.add("ZB");
       
-      System.out.println(merge(list1, list2));
+      //System.out.println(merge(list1, list2));
+      
+       ArrayList<String> unsorted = new ArrayList<String>();
+       //unsorted.add("Zebras"); unsorted.add("Pancakes"); unsorted.add("Bananasw"); unsorted.add("Apples");
+
+        
+        //System.out.println(mergeSort(unsorted));
    }
    
    
